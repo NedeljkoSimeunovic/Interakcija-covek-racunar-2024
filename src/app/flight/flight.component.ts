6dbbbe1e0802215159797b94ea10cbbab2a660fd
NedeljkoSimeunovic <nedeljko.simeunovic.21@singimail.rs>
2024-11-02 15:14:11 +0100

    Finished implementation of details page

--- src/app/flight/flight.component.ts
+++ src/app/flight/flight.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { FlightModel } from '../../models/fligt.model';
 import { WebService } from '../web.service';
-import { JsonPipe, NgIf } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { SafePipe } from '../safe.pipe';
 
 @Component({
   selector: 'app-flight',
   standalone: true,
-  imports: [JsonPipe, HttpClientModule, RouterModule, NgIf],
+  imports: [ HttpClientModule, RouterModule, NgIf,SafePipe],
   templateUrl: './flight.component.html',
   styleUrl: './flight.component.css'
 })
@@ -24,5 +25,9 @@ export class FlightComponent {
       this.webService.getFlightById(id)
       .subscribe(rsp => this.flight = rsp)
     })
+  }
+
+  public getMapUrl() : string{
+    return `https://www.google.com/maps?output=embed&q=${this.flight?.destination}`
   }
 }
